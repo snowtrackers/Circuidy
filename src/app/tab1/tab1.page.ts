ce5c64b60c4a163a4a43725450cb9cff5ae13303
Alex <alexandre.pecikoza@gmail.com>
2019-03-23 01:38:39 +0100

    WIP : form tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { faWheelchair} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  wheelchair = faWheelchair;
+
+  segmentChanged1(ev: any) {
+    console.log('Segment changed', ev);
+  }
+
+  segmentChanged2(ev: any) {
+    console.log('Segment changed', ev);
+  }
+
+  segmentChanged3(ev: any) {
+    console.log('Segment changed', ev);
+  }
+}
+
